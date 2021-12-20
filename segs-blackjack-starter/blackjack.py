a01bede62da2381898d29ee1b7ba8e907836108c
Chris Howarth <49653208+cthow83@users.noreply.github.com>
2021-12-20 23:17:32 +0000

    Update deck.py to reflect named cards Updated blackjack.py to reduce card numbers in Deck.cards

--- segs-blackjack-starter/blackjack.py
+++ segs-blackjack-starter/blackjack.py
@@ -1,34 +1,35 @@
 from src.deck import Deck
 import random
 
+
 def play():
     print('Welcome to BBC Blackjack!')
-    print('For your convinience, a jack, queen or king will be automatically converted')
+    print('For your convenience, a jack, queen or king will be automatically converted')
     print('into its equivalent value. The value of an Ace will be either 11 or 1 depending')
     print('the current value of your hand')
     print()
     scores = []
     print("How many players are there?")
     players = input()
+    deck = Deck()
 
     for i in range(int(players)):
-        deck = Deck()
+
         hand = []
-        total = 0
         stick = False
 
-        total = twist(hand, deck, i)
-        total = twist(hand, deck, i)
+        twist(hand, deck)
+        total = twist(hand, deck)
         print("Player " + str(i + 1) + ", Your hand is", end=" ")
         print(hand)
         print("It is currently worth " + str(total))
-        if (total == 21):
+        if total == 21:
             print("BLACKJACK!!!!")
         else:
-            while (total < 21 and stick == False):
+            while total < 21 and stick == False:
                 option = options()
-                if (option == 1):
-                    total = twist(hand, deck, i)
+                if option == 1:
+                    total = twist(hand, deck)
                     print("Player " + str(i + 1) + ", Your hand is", end=" ")
                     print(hand)
                     print("It is currently worth " + str(total))
@@ -38,24 +39,38 @@ def play():
 
     print("Scores:")
     for i in range(int(players)):
-        print("Player " + str(i+1) + " scored " + str(scores[i]))
+        print("Player " + str(i + 1) + " scored " + str(scores[i]))
 
 
 def return_card(deck):
-    a = random.randint(0, 51)
-    return deck.cards[a]
+    size = len(deck.cards) - 1
+    print(size)
+    a = random.randint(0, size)
+    removed = deck.cards.pop(a)
+    return removed
 
-def twist(hand, deck, player):
+
+def twist(hand, deck):
     card = return_card(deck)
     print("You have been dealt a " + str(card))
-    hand.append(card)
-    if ('A' in hand):
+
+    if card == 'King':
+        value = 10
+    elif card == "Queen":
+        value = 10
+    elif card == "Jack":
+        value = 10
+    else:
+        value = card
+
+    hand.append(value)
+    if 'A' in hand:
         hand.remove('A')
-        if('A' in hand):            #handles multiple Ace situation (only 1 can be max value)
+        if 'A' in hand:  # handles multiple Ace situation (only 1 can be max value)
             hand.remove('A')
             hand.append(1)
         total = sum(hand)
-        if (total < 11):
+        if total < 11:
             total += 11
         else:
             total += 1
@@ -64,18 +79,21 @@ def twist(hand, deck, player):
         total = (sum(hand))
     return total
 
+
 def stick(player, total):
     print("Player " + str(player) + ". You score " + str(total) + ".")
+
 
 def options():
     print("Would you like to hit or stand?")
     print("For hit Press 1")
     print("For stand Press 2")
-    return(int(input()))
+    return int(input())
+
 
 def evaluate(player, total, scores):
     print("Player " + str(player + 1) + ". Your total is " + str(total))
-    if (total <= 21):
+    if total <= 21:
         scores.append(total)
         print("Good Luck")
     else:
@@ -83,6 +101,7 @@ def evaluate(player, total, scores):
         print("YOU ARE BUST. You lose")
     print("Press enter to end your turn")
     input()
+
 
 if __name__ == '__main__':
     play()
---
+++ src/deck.py
@@ -0,0 +1,3 @@
+class Deck:
+    def __init__(self):
+        self.cards = ['A', 'King','Queen','Jack',10, 9, 8, 7, 6, 5, 4, 3, 2, 'A', 'King','Queen','Jack',10, 9, 8, 7, 6, 5, 4, 3, 2,'A', 'King','Queen','Jack',10, 9, 8, 7, 6, 5, 4, 3, 2,'A', 'King','Queen','Jack',10, 9, 8, 7, 6, 5, 4, 3, 2,]
