e958883761d0d8bfeba7a19da9c74038180093ea
Chris Howarth <49653208+cthow83@users.noreply.github.com>
2021-12-21 19:57:44 +0000

    Added notes to blackjack.py Improved presentation of text during game (added time delay)

--- blackjack.py
+++ blackjack.py
@@ -1,21 +1,24 @@
 from src.deck import Deck
 from src.player import Player
 import random
+import time
 
 
 def play():
     # Introduction & set-up
     print('Welcome to BBC Blackjack!')
+    time.sleep(1)
     print('For your convenience, a jack, queen or king will be automatically converted')
     print('into its equivalent value. The value of an Ace will be either 11 or 1 depending')
     print('the current value of your hand')
     print()
+    time.sleep(1)
     print("How many players are there? Max is 5")
     # players = int(input())
     while True:
         try:
             players = int(input())
-            while not players > 1 or not players < 5:
+            while players < 1 or players > 5:
                 print("Please select a number between 1 and 5")
                 players = int(input())
             break
@@ -32,6 +35,7 @@ def play():
     # Play the game
     for player in playerlist:
 
+        time.sleep(1)
         print(player.name + "'s Turn")
 
         stick = False
@@ -41,6 +45,7 @@ def play():
         evaluate(player)
 
         if player.total == 21:
+            time.sleep(1)
             print("BLACKJACK!!!!")
         else:
             while player.total < 21 and stick == False:
@@ -49,16 +54,20 @@ def play():
                     twist(player, deck)
                     evaluate(player)
                 else:
+                    # stand(player)     #Not required in current version. see stand() method below
                     stick = True
         if player.total > 21:
             player.total = 0
 
+        time.sleep(1)
         print(player.name + " scored " + str(player.total))
+        time.sleep(1)
         print("Press enter to end your turn")
         input()
 
     # Dealers turn
     print()
+    time.sleep(1)
     print("Dealers Turn")
 
     dealer = Player("Dealer")
@@ -67,16 +76,21 @@ def play():
     while dealer.total < 17:
         twist(dealer, deck)
     if dealer.total <= 21:
+        time.sleep(1)
         print("Dealer has " + str(dealer.total))
     else:
+        time.sleep(1)
         print("Dealer has bust.")
         dealer.total = 0
 
     # Summary of game
     print()
+    time.sleep(1)
     print("Scores:")
     for player in playerlist:
+        time.sleep(1)
         print(player.name + " scored " + str(player.total))
+    time.sleep(1)
     print(dealer.name + " scored " + str(dealer.total))
 
 
@@ -89,6 +103,7 @@ def return_card(deck):
 
 def twist(player, deck):
     card = return_card(deck)
+    time.sleep(1)
     print(player.name + " has been dealt a " + str(card))
 
     if card == 'King':
@@ -116,11 +131,16 @@ def twist(player, deck):
         player.total = (sum(player.hand))
 
 
-def stick(player, total):
-    print("Player " + str(player) + ". You score " + str(total) + ".")
+# Not currently needed as doesnt add anything to model. Left in for future development
+# def stand(player):
+#     # No more cards, no alteration in total.
+#     print(player.name + ". You score " + str(player.total) + ".")
 
 
 def options():
+    # Provides suitable options for current game set up
+    # Possible to add future options here for rule variations (splits, etc)
+    time.sleep(1)
     print("Would you like to hit or stand?")
     print("For hit Press 1")
     print("For stand Press 2")
@@ -128,11 +148,16 @@ def options():
 
 
 def evaluate(player):
+    # Evaluates the total score of the player when called.
+    # Prints score to terminal
     if player.total <= 21:
+        time.sleep(1)
         print(player.name + ", Your hand is", end=" ")
         print(player.hand)
+        time.sleep(1)
         print("It is currently worth " + str(player.total))
     else:
+        time.sleep(1)
         print("YOU ARE BUST. You lose")
 
 
